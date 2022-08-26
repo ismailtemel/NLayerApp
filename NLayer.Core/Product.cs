dcf12539202eb50ce053d8cf5c39c93767e97875
İsmail Temel <64991440+ismailtemel@users.noreply.github.com>
2022-08-26 15:39:28 +0300

    Düzenleme

--- NLayer.Core/Product.cs
+++ NLayer.Core/Product.cs
@@ -8,6 +8,11 @@ namespace NLayer.Core
 {
     public class Product : BaseEntity
     {
+        //public Product(string name)
+        //{
+        //    Name = name ?? throw new ArgumentNullException(nameof(Name));
+        //}
+
         public string Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
