7cd8836ba90e91ec4f99b712e77dd6509eb958e6
İsmail Temel <64991440+ismailtemel@users.noreply.github.com>
2022-09-18 18:57:24 +0300

    Update

--- NLayer.Core/Models/BaseEntity.cs
+++ NLayer.Core/Models/BaseEntity.cs
@@ -1,10 +1,10 @@
 ﻿namespace NLayer.Core.Models
 {
     // Abstract yazmamızın sebebi base entity den nesne örneği alınmasını önlemek 
-    // Eğer herhangi bir referans tipimizi new anahtar sözcüğünü kullanmadan oluşturamıyorsak biz bunlara soyut nesneler deriz.Abstractlar soyut nesnelerdir çünkü new anahtar sözcüğünü kullanarak yeni bir nesne örneği oluşturamayız. Interfaceler soyut yapılardır new anahtar sözcüğü ile oluşturamayız.
-    // Abstract classlarımız bizim projelerimizde classlarımızdaki ortak property ve methodları tanımladığımız yerlerdir.
+    // Eğer herhangi bir referans tipimizi new anahtar sözcüğünü kullanmadan oluşturamıyorsak biz bunlara soyut nesneler deriz.Abstractlar soyut nesnelerdir çünkü new anahtar sözcüğünü kullanarak yeni bir nesne örneği oluşturamayız.Aynı şekilde Interfacelerde soyut yapılardır ve new anahtar sözcüğü ile oluşturamayız.
+    // Abstract classlarımız projelerimizde classlarımızdaki ortak property ve methodları tanımladığımız yerlerdir.
     // Interfaceler de soyut nesnelerdir genellikle kontrat ve sözleşmelerimizi tanımladığımız yerlerdir.
-    // Interfaceler'le yapabildiklerimizi abstract classlar ile de yapabiliriz çünkü ikisi de soyut yapıdır.
+    // Interfaceler'le yapabildiklerimizi abstract classlar ile de yapabiliriz çünkü ikisi de soyut yapıdır.Tek farkları Abstract class'ı başka bir class'a inherit ederken abstract class içine yazdığımız her bilgiyi almak zorunda değiliz fakat interfacelerde interfacein içine yazdığımız her bilgiyi interface kaltım aldığımız classda uygulamak zorundayız.
     public abstract class BaseEntity
     {
         public int Id { get; set; }
